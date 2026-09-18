0584d7beb5d41d3c4dedd002ce21029efbae4abb
KaisarCode <kaisar@kaisarcode.com>
2026-09-18 00:37:10 -0300

    Add base64 encoding and decoding functionality using FFI

---
+++ projects/demo/src/main.lua
@@ -0,0 +1,31 @@
+local ffi = require("ffi");
+
+local function load(name)
+    local def = io.open("./lib/lib"..name..".cdef", "r");
+    ffi.cdef(def:read("*a")); def:close();
+    return ffi.load("./lib/lib"..name..".so");
+end
+
+local b64 = {};
+local libb64 = load("b64");
+
+function b64.encode(str)
+    local val = libb64.kc_b64_encode(str, #str);
+    if val == nil then return nil; end
+    return ffi.string(val);
+end
+
+function b64.decode(str)
+    local siz = ffi.new("size_t[1]");
+    local val = libb64.kc_b64_decode(str, siz);
+    if val == nil then return nil; end
+    return ffi.string(val, siz[0]);
+end
+
+local input = "";
+if (arg[1] ~= nil) then
+    input = arg[1];
+end
+
+print(b64.encode(input));
+
